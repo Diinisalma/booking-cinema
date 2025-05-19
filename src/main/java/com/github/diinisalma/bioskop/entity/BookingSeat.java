ddecbe821c73ed79f4fc008680d37deb3de5cea7
Diini Salma <op44@fifgroup.co.id>
2025-05-19 11:05:27 +0700

    feat(booking) Add booking_seat entity

---
+++ src/main/java/com/github/diinisalma/bioskop/entity/BookingSeat.java
@@ -0,0 +1,29 @@
+package com.github.diinisalma.bioskop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "t_booking_seat")
+@Data
+@AllArgsConstructor
+public class BookingSeat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
+
+    @ManyToOne
+    @JoinColumn(name = "seat_id", nullable = false)
+    private SeatStudio seat;
+}
